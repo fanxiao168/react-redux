b423d2928388989440659e5a90b1388a1c6befa6
fanxiao168 <fanxiao168@126.com>
2019-07-18 17:44:16 +0800

    init code

---
+++ src/TodoList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TodoList = (props) => {
+
+	const { inputValue ,list, changeInputValue, handleClick ,handleDeleteItemClick} = props;
+	
+	return (
+		<div>
+			<div>
+				<input value={inputValue}  onChange={changeInputValue} />
+				<button onClick={handleClick}>提交</button>
+			</div>
+			<ul>
+				{
+					list.map( (item,index) => {
+					 	return <li onClick={ () => { handleDeleteItemClick(index)} } key={index}>{item}</li>
+					})
+				}	
+			</ul>
+		</div>
+	)
+}
+
+const mapStateToProps = (state) => {
+	return {
+		inputValue:state.inputValue,
+		list:state.list
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		changeInputValue (e) {
+			const action = {
+				type: 'change_input_value',
+				value: e.target.value
+			}
+			dispatch(action);
+		},
+
+		handleClick() {
+			const action = {
+				type: 'add_item'
+			}
+			dispatch(action);
+		},
+
+		handleDeleteItemClick(index) {
+
+			const action = {
+				type: 'delete_item',
+				index
+			}
+			dispatch(action);
+		}
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
---
+++ src/store/reducer.js
@@ -0,0 +1,30 @@
+const defaultState = {
+	inputValue: '',
+	list: []
+}
+
+export default (state = defaultState,action) => {
+	
+	if (action.type === 'change_input_value') {
+		const newState = JSON.parse(JSON.stringify(state));
+		newState.inputValue = action.value;
+		return newState;
+	}
+
+	if (action.type === 'add_item') {
+		const newState = JSON.parse(JSON.stringify(state));
+		newState.list.push(newState.inputValue);
+		newState.inputValue = '';
+		return newState;
+	}
+
+	if (action.type === 'delete_item') {
+		console.log(action);
+		const newState = JSON.parse(JSON.stringify(state));
+		newState.list.splice(action.index);
+		return newState;
+	}
+
+	return state;
+}
+
